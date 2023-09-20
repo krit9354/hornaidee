8da60efcebd924ddfa654e1dc5e37a2e34b2df29
HHJ2520 <markwwe2015@gmail.com>
2023-09-20 23:30:59 +0700

    kub

--- src/pages/register.js
+++ src/pages/register.js
@@ -3,17 +3,18 @@ function Register() {
   return (
     <body >
       <div className='background' >
-        <div className='register_box'>
-        <h1 className='hornaid'>หอไหนD?</h1>
-        <div className='rang'>
-           <h6 className='username'>username</h6>
-          <input type='text' className='input'></input>
-        </div>
+        <div className='containei'>
+          <div className='register_box'>
+            <h1 className='hornaid'>หอไหนD?</h1>
           <div className='rang'>
+           <h6 className='username'>username</h6>
+            <input type='text' className='input'></input>
+          </div>
+            <div className='rang'>
             <h6 className='password'>password</h6>
           <input type='password' className='input'></input><br/>
-        </div>
-        <div className='rang'>
+           </div>
+            <div className='rang'>
             <h6 className='password'>confirmpassword</h6>
           <input type='password' className='input'></input><br/>
         </div>
@@ -24,6 +25,7 @@ function Register() {
             <p>did you already have acount ?</p>
             <i className='rgt_bt'>register</i>
           </div>
+        </div>
         </div>
       </div>
     </body>
