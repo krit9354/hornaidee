698f7a41bbf4cb92f0317e3aa73834b5b0e40d7f
HHJ2520 <markwwe2015@gmail.com>
2023-09-21 00:06:47 +0700

    pop

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,31 @@
 import './Login.scoped.css';
-function Login() {
+function Register() {
   return (
-    <body>
-      <div className='container'>
-        <div className='login_page'>
-          <h1 className='hornaid'>หอไหนD?</h1>
-          <h6 className='username'>username</h6>
-          <input type='text' className='input'></input>
-          <h6 className='password'>password</h6>
+    <body >
+      <div className='background' >
+        <div className='containei'>
+          <div className='register_box'>
+            <h1 className='hornaid'>หอไหนD?</h1>
+          <div className='rang'>
+           <h6 className='username'>username</h6>
+            <input type='text' className='input'></input>
+          </div>
+            <div className='rang'>
+            <h6 className='password'>password</h6>
           <input type='password' className='input'></input><br/>
+           </div>
           <button className='login_bt'>login</button>
-        </div>
-
+      </div>
         <div className='dha'>
           <div className='flex'>
-            <p>don’t have acount ?</p>
+            <p>did you already have acount ?</p>
             <i className='rgt_bt'>register</i>
           </div>
         </div>
+        </div>
       </div>
-
     </body>
   );
 }
-export default Login;
+export default Register;
+
