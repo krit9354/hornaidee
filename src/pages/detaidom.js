78a39b8e30964b7f7127a9233334c4d34020e20e
HHJ2520 <markwwe2015@gmail.com>
2023-09-21 22:09:59 +0700

    popidk

--- src/pages/detaidom.js
+++ src/pages/detaidom.js
@@ -1,11 +1,22 @@
-import React from 'react'
-// import Nav from '/componrnts/nav'
+import './detaidom.scoped.css'
+import Nav from '../components/nav'
 function detaidom() {
   return (
-    <header>
-        
-    </header>
+    <body>
+        <header>
+            <Nav> </Nav>
+        </header>
+        <div className='containei'>
+            <div className='background'>
+                <div className='card'>
+                <img src={"https://th.bing.com/th/id/OIP.HJPHH7ynTGyNaz0t_3esDgAAAA?pid=ImgDet&rs=1"}/>
+                </div>
+                <div className='register_box'>
+                </div>
+            </div>
+        </div>
+    </body>
   )
 }
 
-export default detaidom
+export default detaidom;
