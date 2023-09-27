873c921638279803564a9a2b983b531f5b8ad385
krit9354 <krit9354@gmail.com>
2023-09-28 02:58:28 +0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
-
+import { BorderAllRounded } from "@mui/icons-material"
+import "./footer.scoped.css"
 function Footer (){
   return (
-    <div className="w-full bg-brown">
-        <img src="./img/logo_hornaiD.png" />
-
+    <div className="w-full bg-brown h-56">
+        <div className="image"><img src="./img/logo.png"/></div>
     </div>
   )
 }
